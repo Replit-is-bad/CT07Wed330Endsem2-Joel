22272f1909a780ddd0ec9b2df59f70160c1f37d3
Replit-is-bad <ylchia_hill@hotmail.com>
2025-10-22 08:09:57 +0000

    eee

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,21 @@
+let bird;
+let background, birdImg;
 
+function preload(){
+    bird = loadImage ('assets/bird.png');
+    background = loadImage ('assets/background.png');
+}
+
+function setup(){
+    new Canvas(400, 600);
+    bird = new Sprite();
+    bird.x = width/2;
+    bird.y = 200;
+    bird.width =30;
+    bird.height =30;
+    bird.img = birdImg;
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,29 @@
+let box;
 
+
+function setup() {
+  // Set up the canvas
+  new Canvas(800, 400);
+  background(220);
+  world.gravity.y = 10;
+
+  // box sprite
+  box = new Sprite();
+  box.x = 100;
+  box.y = 100;
+  box.w = 100;
+  box.h = 50;
+  box.color = 'blue';
+  box.collider = "dynamic";
+  box.vel.y =3;
+
+}
+
+function draw(  ) {
+    background(220);
+
+    if(kb.presses('space') || mouse.presses()) {
+        box.vel.y = -5;
+        box.sleeping = false;
+    }
+}
